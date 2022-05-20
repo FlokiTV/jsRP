a57734567d86dc743e88bbab6ceb0c4794f27f8a
FlokiTV <Afn9Wz@OKdzhe.cc>
2022-05-20 14:58:53 -0300

    jsrp core module

--- resources/[jsrp]/[core]/jsrp/fxmanifest.lua
+++ resources/[jsrp]/[core]/jsrp/fxmanifest.lua
@@ -5,17 +5,5 @@ author 'FlokiTV'
 description "jsRP Login"
 version '1.0.0'
 
-ui_page 'nui/index.html' 
--- ui_page 'http://localhost:3000/nui' 
-
-files {
-    "nui/**.js",
-    "nui/**.png",
-    "nui/**.html",
-    "nui/**.json",
-    "nui/**.css",
-}
-
 client_script 'client/*.js'
 server_script 'server/*.js'
-shared_script 'shared.js'
