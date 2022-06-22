12c140759b9c8a5ac78a0293d5a7571238a3cf8c
FlokiTV <Afn9Wz@OKdzhe.cc>
2022-06-22 14:32:44 -0300

    update

--- resources/[jsrp]/jsrp-admin/client/client.js
+++ resources/[jsrp]/jsrp-admin/client/client.js
@@ -1,54 +1,49 @@
-let noclip = false;
-onNet("nc", async () => {
-  noclip = !noclip;
-  FreezeEntityPosition(PlayerPedId(), noclip);
-  SetEntityVisible(PlayerPedId(), !noclip);
-  SetPlayerCanUseCover(PlayerId(), !noclip);
+/// <reference types="@citizenfx/client" />
+const resource = GetCurrentResourceName();
+const jsRP = exports["jsrp"].jsRP;
+const { request } = jsRP();
 
-  let TreadNC = setInterval(() => {
-    let ped = PlayerPedId();
-    let [x, y, z] = GetEntityCoords(ped);
-    let speed = 1.0;
+/*
+  Send RAW HTML with tailwindcss
+*/
+onNet("jsrp-admin:cds", (source, args) => {
+  let [x, y, z] = GetEntityCoords(PlayerPedId());
+  let html = `
+  <div class="w-full h-full flex justify-center items-center">
+    <div class="bg-black text-white p-2">
+      ${x.toFixed(2)}, ${y.toFixed(2)}, ${z.toFixed(2)}
+      <div 
+        class="border text-center mt-2 cursor-pointer" 
+        onclick="rmNUI('jsrp-admin-cds'); emit('nui','focusout')"
+      >
+        close
+      </div>
+    </div>
+  </div>
+`;
+  jsRP("nui").addHTML("jsrp-admin-cds", html);
+});
 
-    const getCamDirection = () => {
-      let heading = GetGameplayCamRelativeHeading() + GetEntityHeading(PlayerPedId());
-      let pitch = GetGameplayCamRelativePitch();
-      let x = -Math.sin((heading * Math.PI) / 180.0);
-      let y = Math.cos((heading * Math.PI) / 180.0);
-      let z = Math.sin((pitch * Math.PI) / 180.0);
-      let len = Math.sqrt(x * x + y * y + z * z);
+/*
+  Prepare NUI function to run on init
+*/
+const initNUI = () => {
+  /*
+    Inject Client HTML
+  */
+  request(resource, "init", GetPlayerServerId(PlayerId()));
+  /*
+    Register NUI's Callbacks
+  */
+  jsRP("nui").on(resource, "screenshot", async (img) => {
+    console.log("screenshot");
+    let send = await request(resource, "send-ss", "teste");
+  });
+};
 
-      if (len !== 0) {
-        x = x / len;
-        y = y / len;
-        z = z / len;
-      }
-      return [x, y, z];
-    };
-
-    const [dx, dy, dz] = getCamDirection();
-
-    SetEntityVelocity(ped, 0.0001, 0.0001, 0.0001);
-
-    if (IsControlPressed(0, 21)) {
-      speed = 5.0;
-    }
-
-    if (IsControlPressed(0, 32)) {
-      x = x + speed * dx;
-      y = y + speed * dy;
-      z = z + speed * dz;
-    }
-
-    if (IsControlPressed(0, 269)) {
-      x = x - speed * dx;
-      y = y - speed * dy;
-      z = z - speed * dz;
-    }
-
-    SetEntityCoordsNoOffset(ped, x, y, z, true, true, true);
-    if (!noclip) {
-      clearInterval(TreadNC);
-    }
-  }, 0);
+// Initializa NUI on jsrp-nui is ready
+onNet("jsrp-nui:ready", initNUI);
+// OR when onClientResourceStart is triggered
+on("onClientResourceStart", (resourceName) => {
+  if (resource == resourceName) initNUI();
 });
